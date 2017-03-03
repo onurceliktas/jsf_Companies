303c7e58355fe33bd88c10be6778bf5f68a8461a
onurceliktas <onurceliktas41@gmail.com>
2017-03-03 06:31:34 +0300

    2nd commit
    2nd commit

--- Companies/src/java/com/companies/beans/DemoBean.java
+++ Companies/src/java/com/companies/beans/DemoBean.java
@@ -6,15 +6,77 @@ import java.io.Serializable;
 import java.util.List;
 import com.companies.pojos.Companies;
 import com.companies.services.CompanyDAO;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 
 @Named(value = "demoBean")
 @SessionScoped
 public class DemoBean implements Serializable {
 
+    private Map<String, Map<String, String>> data = new HashMap<String, Map<String, String>>();
+    private String city;
+    private String district;
+    private Map<String, String> cities;
+    private Map<String, String> districts;
     private Companies company;
 
     public DemoBean() {
         company = new Companies();
+    }
+
+    @PostConstruct
+    public void init() {
+        cities = new HashMap<String, String>();
+        cities.put("Istanbul", "Istanbul");
+        cities.put("Ankara", "Ankara");
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Atasehir", "Atasehir");
+        map.put("Beykoz", "Beykoz");
+        map.put("Kadikoy", "Kadikoy");
+        data.put("Istanbul", map);
+
+        map = new HashMap<String, String>();
+        map.put("Cankaya", "Cankaya");
+        map.put("YeniMahalle", "YeniMahalle");
+        data.put("Ankara", map);
+
+    }
+
+    public Map<String, Map<String, String>> getData() {
+        return data;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public Map<String, String> getCities() {
+        return cities;
+    }
+
+    public Map<String, String> getDistricts() {
+        return districts;
+    }
+    
+    public void onCityChange() {
+        if(city !=null && !city.equals(""))
+            districts = data.get(city);
+        else
+            districts = new HashMap<String, String>();
     }
 
     public void save() {
@@ -39,6 +101,11 @@ public class DemoBean implements Serializable {
 
     public Companies getCompany() {
         return company;
+    }
+
+    public List<Companies> filterCompanies() {
+        CompanyDAO companyDAO = new CompanyDAO();
+        return companyDAO.filterCompanies();
     }
 
     public void setCompany(Companies company) {
--- Companies/src/java/com/companies/services/CompanyDAO.java
+++ Companies/src/java/com/companies/services/CompanyDAO.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.companies.pojos.Companies;
 import com.companies.util.HibernateUtil;
 import org.hibernate.Query;
+import com.companies.beans.DemoBean;
+import javax.print.DocFlavor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -14,6 +16,25 @@ import org.hibernate.Transaction;
  * @author onur
  */
 public class CompanyDAO {
+
+    private Integer companyId;
+    private DemoBean dm;
+
+    public DemoBean getDm() {
+        return dm;
+    }
+
+    public void setDm(DemoBean dm) {
+        this.dm = dm;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
 
     public void save(Companies company) {
         SessionFactory sessionFactory;
@@ -26,6 +47,8 @@ public class CompanyDAO {
         company.setCompanyId(null);
         company.setCompanyName("");
         company.setCompanyWeb("");
+        company.setCompanyCity("");
+        company.setCompanyDistrict("");
     }
 
     public void update(Companies company) {
@@ -33,12 +56,15 @@ public class CompanyDAO {
         sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
         Transaction ts = session.beginTransaction();
+
         session.update(company);
         ts.commit();
         company.setCompanyAddress("");
         company.setCompanyId(null);
         company.setCompanyName("");
         company.setCompanyWeb("");
+        company.setCompanyCity(null);
+        company.setCompanyDistrict(null);
     }
 
     public void delete(Companies company) {
@@ -52,6 +78,8 @@ public class CompanyDAO {
         company.setCompanyId(null);
         company.setCompanyName("");
         company.setCompanyWeb("");
+        company.setCompanyCity("");
+        company.setCompanyDistrict("");
 
     }
 
@@ -59,11 +87,25 @@ public class CompanyDAO {
         List<Companies> list = new ArrayList<>();
         Session ses = HibernateUtil.getSessionFactory().openSession();
         ses.beginTransaction();
-        Query qu = ses.createQuery("from Companies");
+        Query qu = ses.createQuery("from  Companies");
         list = qu.list();
         ses.getTransaction().commit();
         ses.close();
         return list;
     }
+
+    public List<Companies> filterCompanies() {
+        List<Companies> list = new ArrayList<>();
+        Session ses = HibernateUtil.getSessionFactory().openSession();
+        ses.beginTransaction();
+        /////
+        Query qu = ses.createQuery("from Companies where company_ID like :companyId")
+                .setParameter("companyId", "%" + companyId + "%");
+        list = qu.list();
+        ses.getTransaction().commit();
+        ses.close();
+        return list;
+    }
+
 
 }
